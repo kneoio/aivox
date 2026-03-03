724fedeaf0bb4dd303c1fc62257c25939c48e29a
kaira <justaidajam@gmail.com>
2026-03-03 18:26:46 +0000

    #

--- src/main/java/com/semantyca/aivox/rest/StreamingResource.java
+++ src/main/java/com/semantyca/aivox/rest/StreamingResource.java
@@ -20,7 +20,7 @@ public class StreamingResource {
     private StreamingService streamingService;
     
     public void setupRoutes(Router router) {
-        String path = "/api/stream";
+        String path = "/stream";
         
         router.route(HttpMethod.GET, path + "/:brand/master.m3u8").handler(this::getMasterPlaylist);
         router.route(HttpMethod.GET, path + "/:brand/stream.m3u8").handler(this::getPlaylist);
